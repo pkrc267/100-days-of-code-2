b02622d0444c976bfccdfbfbc51da22cb49a780a
Prakhar Yadav <pkrc267@gmail.com>
2018-01-15 23:40:03 +0530

    8

--- stringRotation.cpp
+++ stringRotation.cpp
@@ -1,3 +1,16 @@
+//this program checks if one string is rotation of the other or not
+/*
+main concept:
+let string be s1 : waterbottle
+its rotaton be s2: terbottlewa
+now the point is, the main string has been in two parts
+x = wa
+y = terbottle
+i.e. s1 = xy
+the rotated string s2 = yx, will surely be a substring of xyxy = s1+s1
+just check if the it is a substring of self concatenated string s1
+*/
+
 #include <iostream>
 #include <string>
 
